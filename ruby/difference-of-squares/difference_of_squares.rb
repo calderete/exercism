66cf38da53091f4e8111642cc9439ea1ca4f8648
Charles <codprestige10@gmail.com>
2017-01-17 13:19:05 -0500

    finished diff or squares test

---
+++ ruby/difference-of-squares/difference_of_squares.rb
@@ -0,0 +1,28 @@
+module BookKeeping
+	VERSION = 3
+end
+
+class Squares
+
+  def initialize(n)
+  	@number_list = (0..n).to_a 
+  end
+
+  def square_of_sum
+  	@square = @number_list.reduce(:+)**2
+    return @square
+  end
+
+  def sum_of_squares
+  	@sum = @number_list.map { |n| n**2 }.reduce(:+)
+  	return @sum
+  end
+
+  def difference
+  	square_of_sum
+  	sum_of_squares
+  	result = @square - @sum
+  	return result
+  end
+
+end
